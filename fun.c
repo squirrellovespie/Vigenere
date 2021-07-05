93f01c482ba2e4bf2d161b0263fc9c206e130b91
squirrellovespie <71578210+squirrellovespie@users.noreply.github.com>
2021-07-05 20:28:12 +0530

    Comments added

--- fun.c
+++ fun.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 #include<string.h>
+/*read(f,inp)
+1. Opens input.txt in read mode
+2. Traverses through the text until EOF is reached and assigns each character to inp
+3. Adds a '\0' to the end of inp
+4. Closes the file
+*/
 void read(FILE *f,char *input){
     char ch;int i;
     f=fopen("input.txt","r");
@@ -7,11 +13,22 @@ void read(FILE *f,char *input){
     input[i]='\0';
     fclose(f);
 }
+/*write(f,inp)
+1. Opens output.txt in write mode(Rewrites previous text)
+2. Prints out to the file
+3. Closes the file
+*/
 void write(FILE *f,char *output){
     f=fopen("output.txt","w");
     fprintf(f,"%s",output);
     fclose(f);
 }
+/*capncheck(inp/key)
+1. Traverses through the input string
+2. Returns 0 if any character is a special character(not an alphabet or a space or a newline)
+3. Capitalises the small alphabets
+4. Returns 1 at the end
+*/
 int capncheck(char *input){
     for(int i=0;input[i]!='\0';i++){
         if(!(((input[i]>='A'&&input[i]<='Z')||(input[i]>='a'&&input[i]<='z'))||input[i]==' '||input[i]=='\n')) return 0;
@@ -19,31 +36,42 @@ int capncheck(char *input){
     }
     return 1;
 }
+/*keypad(inp,key)
+1. Adds the key to itself until input text length is reached
+2. Adds a '\0' to the end of key
+*/
 void keypad(char *in,char *k){
-    int check=0,klen=strlen(k),inlen=strlen(in),flag=0,icopy=klen;
-    for(int i=klen;i<inlen;i++){//Start loop from kcount and iterate until the length of plain text
-        if(flag==1){
-            i--;
-            inlen--;
-            flag=0;
+    int check=0,klen=strlen(k),inlen=strlen(in),flag=0,icopy=klen;//icopy replicates i unless the input has ' ' or '\n'(only used with input text)
+    for(int i=klen;i<inlen;i++){//Start loop from klen and iterate until input length
+        if(flag==1){//If '\n' or ' ' was encountered
+            i--;//Key index must go back
+            inlen--;//Input text length must decrease
+            flag=0;//Flag is re-initialized to 0
         }
-        if(in[icopy]=='\n'||in[icopy]==' '){
-            flag=1;
-            icopy++;
-            continue;
+        if(in[icopy]=='\n'||in[icopy]==' '){//Key padding must not take place if input has '\n' or ' '
+            flag=1;//'\n' or ' ' has been encountered
+            icopy++;//icopy must increase irrespective
+            continue;//Restarts the for loop
         }
         k[i]=k[check];//Store each value of the inputted key
         check+=1;//Increment check to access the next element of the inputted key
         if(check==klen) check=0;//check becomes 0 at the end of the inputted key
-        icopy++;
+        icopy++;//Increments after each iteration(To go through input text)
     }
     k[inlen]='\0';
 }
+/*encrypt(inp,out,key)
+1. Calls keypad() to create final key
+2. Traverses through inp and applies encryption formula if the character is an alphabet
+3. Otherwise(' ' or '\n') output character is the same as input
+4. icopy is used to adjust the absence of ' ' or '\n' in the key
+5. '\0' is added to the end of out
+*/
 void encrypt(char *in,char *out,char *k){
-    keypad(in,k);int i,icopy=0;
+    keypad(in,k);int i,icopy=0;//Padded key is created
     for(i=0;in[i]!='\0';i++){
         if(in[i]!=' '&&in[i]!='\n'){
-            out[i]=(char)(((((int)in[i]-65)+((int)k[icopy]-65))%26)+65);
+            out[i]=(char)(((((int)in[i]-65)+((int)k[icopy]-65))%26)+65);//Encryption Formula:E=(P+K)%26
         }
         else{
             out[i]=in[i];
@@ -53,6 +81,8 @@ void encrypt(char *in,char *out,char *k){
     }
     out[i]='\0';
 }
+/*Similar to encrypt()
+If Decryption Formula(D=(E-K)%26) becomes negative, add 26*/
 void decrypt(char *in,char *out,char *k){
     keypad(in,k);int i,icopy=0;
     for(i=0;in[i]!='\0';i++){
--- menu.c
+++ menu.c
@@ -2,6 +2,14 @@
 #include<stdlib.h>
 #include"fun.h"
 #include<string.h>
+/*VARIABLES:
+1. f = file pointer
+2. inp = points to the text from input.txt
+3. out = points to the text to be written in output.txt
+4. key = points to the key received from stdin
+5. choice = stores choice of Encryption/Decryption/Exit
+6. flag = Used to exit the while loop
+*/
 int main(){
     while(1){
         puts("");
@@ -10,19 +18,19 @@ int main(){
         printf("VIGENERE CIPHER\n1. Encryption\n2. Decryption\n3. Exit\n\nEnter choice: ");
         scanf("%d",&choice);
         read(f,inp);
-        if(choice==1||choice==2){
+        if(choice==1||choice==2){//Only if Exit is not chosen
             printf("Enter key: ");
             scanf("%s",key);
-            if(strlen(key)>strlen(inp)){
-                printf("The key length cannot be greater than the length of the text. Please try again.");
-                continue;
+            if(strlen(key)>strlen(inp)){//Key length must be <= Text length
+                puts("The key length cannot be greater than the length of the text. Please try again.");
+                continue;//Restarts the while loop
             }
-            if(capncheck(inp)&&capncheck(key)){
-                f=fopen("input.txt","w");
+            if(capncheck(inp)&&capncheck(key)){//Capitalising inp and key
+                f=fopen("input.txt","w");//Rewriting input text after capitalisation
                 fprintf(f,"%s",inp);
                 fclose(f);
             }
-            else{
+            else{//capncheck() returns 0 if any other symbol is present
                 puts("Sorry! The file and key must only contain alphabets. Please try again.");
                 continue;
             }
@@ -39,14 +47,14 @@ int main(){
                 puts("Decrypted successfully!");
                 break;
             case 3:
-                flag=1;
+                flag=1;//To exit the while loop
                 break;
             default:
                 puts("Invalid choice.");
         }
-        free(inp);free(out);free(key);
-        inp,out,key=NULL,NULL,NULL;
-        if(flag==1) break;
+        free(inp);free(out);free(key);//Freeing memory
+        inp,out,key=NULL,NULL,NULL;//Assigning dangling pointers to NULL
+        if(flag==1) break;//Exiting while loop if choice is 3
     }
     return 0;
 }
